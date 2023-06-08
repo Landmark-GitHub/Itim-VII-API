cc86ba961c42936bcf559e8b3ce53a20ad4f9b2d
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 11:46:37 +0700

    Frist Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const app = express();
+const dreamitim = mysql.createPool(process.env.DATABASE_URL)
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to DreamItim API')
+})
+
+app.get('/itim', (req, res) => {
+    dreamitim.query(
+        'SELECT * FROM `itim`',
+        function (err, results, fields) {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error retrieving data from the database table itim');
+            } else {
+                res.status(200).json(results);
+            }
+        }
+      );
+})
+
+app.listen(process.env.PORT || 3000);
+// dreamitim.end()
