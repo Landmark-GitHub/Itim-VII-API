58d9d8b260dafa8f2f5dfdaafdbba669a64b1d7f
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 23:29:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -240,4 +240,28 @@ app.get('/typesItim', (req, res) => {
     });
 })
 
+app.get('/newItim', (req, res) => {
+    const { params } = req.query;
+    const [date, name, typeitim] = params;
+  
+    let query = 'SELECT ';
+  
+    if (date && name && typeitim) {
+      query += 'name, typeItim, SUM(quantity) AS total_quantity FROM `requisition` WHERE `date` = ? AND `name` = ? AND `typeitim` = ?';
+    } else if (date && name) {
+      query += 'name, typeItim, SUM(quantity) AS total_quantity FROM `requisition` WHERE `date` = ? AND `name` = ? GROUP BY `typeitim`';
+    } else if (date) {
+      query += 'name, typeItim, SUM(quantity) AS total_quantity FROM `requisition` WHERE `date` = ? GROUP BY `name`, `typeitim`';
+    }
+  
+    dream_itim.query(query, [date, name, typeitim], function (err, results, fields) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error', date, name, typeitim });
+      }
+      const formattedResults = JSON.stringify(results, null, 2); // Format the JSON response
+      return res.send(formattedResults);
+    });
+})
+
 app.listen(process.env.PORT || 3001);
