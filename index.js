d01649b2d799c53ce6f953224197e772ff6f010c
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 18:33:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const dreamitim = mysql.createPool(process.env.DATABASE_URL)
 
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://itim-vii.vercel.app',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
