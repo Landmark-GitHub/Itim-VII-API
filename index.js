88afc654075443b836f0d9f6dddfb74c8b3d8fc6
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 16:53:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -42,26 +42,8 @@ app.get('/members', (req, res) => {
     );
 })
 
-// app.post('/postMembers', (req, res) => {
-//     const { member_name, member_phone, member_idcard } = req.body;
-
-//     dreamitim.query(
-//       'INSERT INTO `member` (`member_name`, `member_phone`, `member_idcard`) VALUES (?, ?, ?)',
-//       [member_name, member_phone, member_idcard],
-//       function (err, results, fields) {
-//         if (err) {
-//           console.error(err);
-//           res.status(500).json({ message: 'Error' });
-//           return;
-//         } else {
-//           console.log(results);
-//           res.status(200).json({ message: 'Add Member Success' });
-//         }
-//       }
-//     );
-// })
-
 app.post('/postMembers', cors(), (req, res) => {
+    
     const { member_name, member_phone, member_idcard } = req.body;
 
     dreamitim.query(
