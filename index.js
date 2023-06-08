58c02cdd8405fc8947b253863c99aa6f3c73142f
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 16:59:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -42,26 +42,32 @@ app.get('/members', (req, res) => {
     );
 })
 
-app.post('/postMembers', cors(), (req, res) => {
+app.post('/postMembers', cors(), async (req, res) => {
+    try {
+        res.setHeader('Access-Control-Allow-Origin', 'https://itim-vii.vercel.app'); // เพิ่มหัวข้อ 'Access-Control-Allow-Origin' ในการตอบกลับ
 
-    const { member_name, member_phone, member_idcard } = req.body;
+        const { member_name, member_phone, member_idcard } = req.body;
 
-    dreamitim.query(
-        'INSERT INTO `member` (`member_name`, `member_phone`, `member_idcard`) VALUES (?, ?, ?)',
-        [member_name, member_phone, member_idcard],
-        function (err, results, fields) {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ message: 'Error' });
-                return;
-            } else {
-                console.log(results);
-                res.status(200).json({ message: 'Add Member Success' });
-            }
-        }
-    );
+        const result = await new Promise((resolve, reject) => {
+            dreamitim.query(
+                'INSERT INTO `member` (`member_name`, `member_phone`, `member_idcard`) VALUES (?, ?, ?)',
+                [member_name, member_phone, member_idcard],
+                function (err, results, fields) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(results);
+                    }
+                }
+            );
+        });
+        console.log(result);
+        res.status(200).json({ message: 'Add Member Success' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error' });
+    }
 });
-
 
 app.get('/requisition', async (req,res) => {
     const { date, name } = req.query;
